d996ecf922138fd366420111c5cdf54690c09b9d
camillesauer <camille-sauer@hotmail.fr>
2021-06-02 16:16:23 +0200

    ok

--- api.py
+++ api.py
@@ -1,7 +1,21 @@
+import spacy
+from spacytextblob.spacytextblob import SpacyTextBlob
+import requests
 from fastapi import FastAPI
-from main import fonction_nlp, square
+
 
 app = FastAPI()
+
+def fonction_nlp(user_input):
+    nlp = spacy.load('en_core_web_sm')
+    text = 'Today is an amazing day!'
+    spacy_text_blob = SpacyTextBlob("Text", text)
+
+    nlp.add_pipe('spacytextblob')
+    doc = nlp(user_input)
+
+    return {'Polarity:': round(doc._.polarity, 2), 'Subjectivity:': round(doc._.subjectivity, 2)}
+
 
 @app.get("/test")
 async def root():
